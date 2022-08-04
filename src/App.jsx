736a8821dc8ad6e3a83b3960baa117a1dc6c3f27
Devil5614737 <kaushiksheel9@gmail.com>
2022-08-04 17:54:57 +0530

    protected the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Intro from './pages/Intro';
 import Login from './pages/login';
 import './styles/app.css';
@@ -6,16 +6,29 @@ import {Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import SearchResults from './pages/SearchResults';
 import WhoIsWatching from './pages/WhoIsWatching';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase/config';
 
 function App() {
+  const [user,setUser]=useState();
+  useEffect(()=>{
+    onAuthStateChanged(auth,(data)=>{
+    if(data) setUser(data);
+   
+    })
+      
+    
+    },[])
+
+
   return (
   <>
   <Routes>
     <Route  path='/' element={<Intro/>}/>
     <Route path='/login' element={<Login/>}/>
-    <Route path='/home' element={<Home/>}/>
-    <Route path='/results' element={<SearchResults/>}/>
-    <Route path='/whoiswatching' element={<WhoIsWatching/>}/>
+    <Route path='/home' element={user?<Home/>:<Login/>}/>
+    <Route path='/results' element={user?<SearchResults/>:<Login/>}/>
+    <Route path='/whoiswatching' element={user?<WhoIsWatching/>:<Login/>}/>
   </Routes>
 
   </>
